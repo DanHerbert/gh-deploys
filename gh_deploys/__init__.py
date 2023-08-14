1f00f0a49922a1aa1b54db5883a3bd36e37d08f2
Dan Herbert <gh-deploy-dan@hrbrt.co>
2023-08-14 16:48:21 -0700

    Handle my relative URL paths and add custom 404

--- gh_deploys/__init__.py
+++ gh_deploys/__init__.py
@@ -8,12 +8,19 @@ from .config import get_config
 config = get_config()
 app = Flask(__name__)
 app.logger.setLevel(logging.getLevelName(config['log_level']))
-webhook = Webhook(app, secret=config['secret'])
+webhook = Webhook(app, secret=config['secret'], endpoint=f'{config["root_path"]}/postreceive')
 
 @app.route("/")
+@app.route(config["root_path"])
+@app.route(f'{config["root_path"]}/')
 def root_path_handler():
     app.logger.info(f'Handling {request.url}')
     return "It works!"
+
+@app.errorhandler(404)
+def page_not_found(e):
+    app.logger.info(f'Handling {request.url}')
+    return "Not found!"
 
 @webhook.hook(event_type="push")
 def on_push(data):
