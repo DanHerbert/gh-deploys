7426cd2bfdc62f64e05b5a47bff748dafbdf831b
Dan Herbert <gh-deploy-dan@hrbrt.co>
2023-08-14 18:32:40 -0700

    Handle more Github events in a better way
    Also fix a bunch of pylint errors

--- gh_deploys/__init__.py
+++ gh_deploys/__init__.py
@@ -1,3 +1,5 @@
+"""App to handle Github webhooks."""
+
 import sys
 import logging
 
@@ -8,25 +10,37 @@ from .config import get_config
 config = get_config()
 app = Flask(__name__)
 app.logger.setLevel(logging.getLevelName(config['log_level']))
-webhook = Webhook(app, secret=config['secret'], endpoint=f'{config["root_path"]}/postreceive')
+webhook = Webhook(app, endpoint=f'{config["root_path"]}/postreceive')
+webhookLogger = logging.getLogger('webhook')
+webhookLogger.setLevel(logging.getLevelName(config['log_level']))
 
 @app.route("/")
 @app.route(config["root_path"])
 @app.route(f'{config["root_path"]}/')
 def root_path_handler():
+    """Handle requests to root page to show proof of life in testing."""
     app.logger.info(f'Handling {request.url}')
     return "It works!"
 
 @app.errorhandler(404)
-def page_not_found(e):
+def page_not_found():
+    """Custom 404 to make diagnosing bad routes easier."""
     app.logger.info(f'Handling {request.url}')
     return "Not found!"
 
 @webhook.hook(event_type="push")
 def on_push(data):
+    """Handle Github push events."""
     app.logger.info(f'Handling {request.url}')
-    app.logger.info(f'Got push with: {data}')
+    app.logger.info(f'Push to {data["repository"]["full_name"]} with {data["ref"]}')
 
+@webhook.hook(event_type="ping")
+def on_ping():
+    """Handle Github ping events."""
+    app.logger.info(f'Handling {request.url}')
+    app.logger.info('Got ping request')
+
+# This only happens if running the script directly. Flask/gunicorn ignore this.
 if __name__ == "__main__":
     app.run(host="0.0.0.0",
             port=config['listen_port'],
