e5e455cb5ca8bd560bd7251c0e0251afdc2c2371
Dan Herbert <gh-deploy-dan@hrbrt.co>
2023-08-22 13:15:37 -0700

    Run config.py through pyformat

--- gh_deploys/config.py
+++ gh_deploys/config.py
@@ -15,6 +15,7 @@ class Project:
         self.deploy_branch = deploy_branch
         self.command = command
 
+
 @dataclass
 class Config:
     def __init__(self, root_path, log_level, secret, projects):
@@ -23,18 +24,19 @@ class Config:
         self.secret = secret
         self.projects = projects
 
+
 def get_config():
     """Gets the config object from disk."""
     with open(CONFIG_PATH, mode='rt', encoding='utf-8') as file_handle:
         conf_dict = yaml.safe_load(file_handle)
         projects = []
-        for project_dict in conf_dict["projects"]:
+        for project_dict in conf_dict['projects']:
             projects.append(Project(
-                    project_dict["repo_name"],
-                    project_dict["deploy_branch"],
-                    project_dict["command"]))
+                project_dict['repo_name'],
+                project_dict['deploy_branch'],
+                project_dict['command']))
         return Config(
-                conf_dict["root_path"],
-                logging.getLevelName(conf_dict["log_level"]),
-                conf_dict["secret"],
-                projects)
+            conf_dict['root_path'],
+            logging.getLevelName(conf_dict['log_level']),
+            conf_dict['secret'],
+            projects)
