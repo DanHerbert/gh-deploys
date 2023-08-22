567e66a08f6f9956dc9d193765645a2ca17c1151
Dan Herbert <gh-deploy-dan@hrbrt.co>
2023-08-22 13:16:36 -0700

    Run setup.py through pyformat

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="gh-deploys",
-    version="0.0.1",
-    description="Deployments based on Github Repo changes.",
-    author="Dan Herbert",
-    author_email="gh-deploy-dan@hrbrt.co",
-    license="Apache 2.0",
-    packages=["gh_deploys"],
-    install_requires=["github-webhook", "pyyaml", "gunicorn",],
+    name='gh-deploys',
+    version='0.0.1',
+    description='Deployments based on Github Repo changes.',
+    author='Dan Herbert',
+    author_email='gh-deploy-dan@hrbrt.co',
+    license='Apache 2.0',
+    packages=['gh_deploys'],
+    install_requires=['github-webhook', 'pyyaml', 'gunicorn',],
 )
