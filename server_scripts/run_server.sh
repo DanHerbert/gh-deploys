e4e1cdd91a1c233e869769a468d3413dd6bf58de
Dan Herbert <gh-deploy-dan@hrbrt.co>
2025-08-28 10:41:23 -0400

    Migrate from setup.py to pyproject.toml
    setup.py is deprecated in recent versions of Python.

--- server_scripts/run_server.sh
+++ server_scripts/run_server.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-set -ev
 
-cd /home/dan/code/gh-deploys/
-./env/bin/gunicorn 'gh_deploys:app'
+SCRIPT_DIR=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)
+
+set -ex
+
+cd "$SCRIPT_DIR/.."
+
+if [ ! -d ./venv/ ]; then
+    echo 'Missing python /venv/ dir.'
+    exit 1
+fi
+
+./venv/bin/gunicorn 'src:app'
